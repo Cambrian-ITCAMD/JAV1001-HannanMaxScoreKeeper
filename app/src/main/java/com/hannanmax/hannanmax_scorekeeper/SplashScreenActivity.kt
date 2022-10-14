616f586404dc94306006986f20b77f8c7a48e01c
hannanmax <A00265130@mycambrian.ca>
2022-10-14 19:42:49 -0400

    Created Lab 4 Project in android studio

---
+++ app/src/main/java/com/hannanmax/hannanmax_scorekeeper/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.hannanmax.hannanmax_scorekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
