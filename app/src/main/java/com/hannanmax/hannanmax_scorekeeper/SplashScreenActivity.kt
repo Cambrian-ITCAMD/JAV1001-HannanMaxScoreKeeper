92c6962d83a791d516f6f30a4b7b6eab22802000
hannanmax <A00265130@mycambrian.ca>
2022-10-17 00:58:53 -0400

    Added SpalshScreenActivity

--- app/src/main/java/com/hannanmax/hannanmax_scorekeeper/SplashScreenActivity.kt
+++ app/src/main/java/com/hannanmax/hannanmax_scorekeeper/SplashScreenActivity.kt
@@ -14,6 +14,6 @@ class SplashScreenActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 1500)
     }
 }
